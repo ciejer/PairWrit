46ce31ee181aa810725eb1d0b00cf0e38dee7066
Chris Jenkins <chris@chrisjenkins.nz>
2024-08-31 20:47:11 +1200

    style: update header component styles and structure for improved design

--- pairwrit/src/components/Header.vue
+++ pairwrit/src/components/Header.vue
@@ -1,23 +1,4 @@
 <template>
-    <header class="bg-blue-500 text-white p-4">
-      <h1 class="text-2xl">PairWrit</h1>
-    </header>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    name: 'Header'
-  });
-  </script>
-  
-  <style scoped>
-  header {
-    background-color: #1e3a8a;
-    color: white;
-  }
-  </style><template>
   <header class="bg-gray-800 text-white p-4 shadow-md">
     <div class="container mx-auto flex justify-between items-center">
       <h1 class="text-2xl font-bold">My Application</h1>
